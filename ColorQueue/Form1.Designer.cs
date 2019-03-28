2ebf7b662e8e379d1aa6e8e232e3c47ab0f27506
gulsahduman <46864458+gulsahduman@users.noreply.github.com>
2019-03-28 23:48:45 +0300

    Add files via upload

---
+++ ColorQueue/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace ColorQueue
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(432, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(12, 27);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(78, 79);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(180, 27);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(78, 79);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(264, 27);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(78, 79);
+            this.panel4.TabIndex = 2;
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(96, 27);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(78, 79);
+            this.panel5.TabIndex = 2;
+            // 
+            // panel6
+            // 
+            this.panel6.Location = new System.Drawing.Point(348, 27);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(78, 79);
+            this.panel6.TabIndex = 2;
+            // 
+            // panel7
+            // 
+            this.panel7.Location = new System.Drawing.Point(432, 27);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(78, 79);
+            this.panel7.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 177);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Colourful";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+    }
+}
+
---
+++ ColorQueue/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorQueue
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Colors = new Queue<Color>();
+            Colors.Enqueue(Color.Red);
+            Colors.Enqueue(Color.Blue);
+            Colors.Enqueue(Color.Pink);
+            Colors.Enqueue(Color.Purple);
+            Colors.Enqueue(Color.Green);
+            Colors.Enqueue(Color.Yellow);
+        }
+        public Queue<Color> Colors { get; set; }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var c = Colors.Dequeue();
+            Colors.Enqueue(c);
+            Form1_Load(null, null);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var index = 0;
+            foreach (var ctrl in this.Controls)
+            {
+                if (ctrl is Panel)
+                {
+                    (ctrl as Panel).BackColor = Colors.ToList()[index]; // Tüm IEnumerable nesneler temelde list olabilir.
+                    index++;
+                }
+            }
+        }
+    }
+    
+}
+
